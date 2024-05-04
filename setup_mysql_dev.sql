62e0e50d032d9c61aee49986135f884b3c9d90b6
kmlsd <kmlsd4757@gmail.com>
2024-05-04 14:55:08 +0000

    a setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,11 @@
 --   Database hbnb_dev_db.
 --   User hbnb_dev with password hbnb_dev_pwd in localhost.
 --   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance_schema.
+--   Grants SELECT privilege for hbnb_dev on performance_schem
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+USE `hbnb_dev_db`;
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
