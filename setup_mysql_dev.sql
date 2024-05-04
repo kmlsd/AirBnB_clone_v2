d3bc91b7055ae642653f28e3459a2794d8c3649d
kmlsd <kmlsd4757@gmail.com>
2024-05-04 14:59:24 +0000

    a setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,10 +3,22 @@
 --   User hbnb_dev with password hbnb_dev_pwd in localhost.
 --   Grants all privileges for hbnb_dev on hbnb_dev_db.
 --   Grants SELECT privilege for hbnb_dev on performance_schem
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-USE `hbnb_dev_db`;
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
 
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
-	IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
-	IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
