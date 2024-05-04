89e86a32370b375fdcf8c465573f6f8abfb029f0
kmlsd <kmlsd4757@gmail.com>
2024-05-04 22:21:33 +0000

    update user .

--- models/user.py
+++ models/user.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
-#!/usr/bin/python3
 """Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
@@ -21,13 +18,11 @@ class User(BaseModel, Base):
         password (sqlalchemy String): The user's password.
         first_name (sqlalchemy String): The user's first name.
         last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
+  
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+   
