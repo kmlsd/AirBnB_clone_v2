2a237ac44e51d9aa8e017927c0865abb91cd7647
kmlsd <kmlsd4757@gmail.com>
2024-05-04 22:01:10 +0000

    update file to use  SQLAlchemy

--- models/base_model.py
+++ models/base_model.py
@@ -7,24 +7,33 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 from datetime import datetime
 
+Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            updat = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            creat= datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = updat
-            kwargs['created_at'] = creat
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs.get("updated_at"):
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f'
+                                                     )
+        if kwargs.get("created_at"):
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f'
+                                                     )
+        if kwargs.get("__class__"):
             del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.__dict__.update(kwargs)
+
 
     def __str__(self):
         """Returns a string representation of the instance"""
