b737603ad85e142958cb0fdcba02335bb391d075
kmlsd <kmlsd4757@gmail.com>
2024-05-18 14:58:34 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,45 @@
 #!/usr/bin/env bash
 # Sets up the web servers for the deployment of web_static.
-
 # Install Nginx if not already installed
 sudo apt update -y >/dev/null 2>&1
-sudo apt install nginx -y >/dev/null 2>&1
-
-# Create required director  if it doesn’t already exist
+sudo apt install nginx -y
 
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
 
-# Create fake index.html file
-sudo echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t fake School_file\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s -f /data/web_static/releases/test /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Create the symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Change ownership of files and folders inside of /data folder
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
-sudo echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Add alias to serve the content of /data/web_static/current to hbnb_static
-sudo sed -i '/location \//i \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo  nginx -t
+    error_page 404 /404.html;
+location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+#sudo sed -i '/listen 80 default_server/a location / hbnb_static {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+#sudo  nginx -t
 # Restart the nginx service
 sudo service nginx restart
