683a157dbcbb9ab1ca2fa035a777baae6b9dc30c
kmlsd <kmlsd4757@gmail.com>
2024-05-19 16:08:21 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,25 @@
-#!/usr/bin/env python3
+
+#!/usr/bin/python3
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
 
 from datetime import datetime
 from fabric.api import *
-import os
+
 
 def do_pack():
+    """
+    making an archive on web_static folder
+    """
 
-    local('sudo mkdir -p versions')
     time = datetime.now()
-    time_str = time.strftime("%Y%m%d%H%M%S")
-    local('sudo tar -cvzf versions/web_static_time_str.tgz web_static')
-    
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
 
